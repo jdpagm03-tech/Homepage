216bd53375ac75de4c412ecbfbfd5ceb9aac380c
jdpagm03-tech <jdpagm03@gmail.com>
2026-03-14 22:03:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ import { initWeather } from "./weather.js";
 import { renderServices } from "./services.js";
 import { enableDragAndDrop } from "./dragdrop.js";
 import { initSearch } from "./search.js";
+import { initModal } from "./modal.js";
 
 initClock();
 initWeather();
 renderServices();
 enableDragAndDrop(renderServices);
 initSearch();
+initModal();
