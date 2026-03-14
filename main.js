67ecbf967d10f4e141f415be239d66f03a4f6631
jdpagm03-tech <jdpagm03@gmail.com>
2026-03-14 21:56:51 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+import { initClock } from "./clock.js";
+import { initWeather } from "./weather.js";
+import { renderServices } from "./services.js";
+import { enableDragAndDrop } from "./dragdrop.js";
+import { initSearch } from "./search.js";
+
+initClock();
+initWeather();
+renderServices();
+enableDragAndDrop(renderServices);
+initSearch();
